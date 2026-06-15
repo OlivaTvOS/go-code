d8f798237e26b3c34313150e3c840b81172d5002
Artur <olivaovso@gmail.com>
2026-06-15 18:02:53 -0300

    var-type

--- hello.go
+++ hello.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Var Test")
+
+	var name string = "Artur"
+	var age = 18
+	var version float32 = 0.1
+
+	fmt.Println("\nHello, sr.", name, "you are", age)
+	fmt.Println("\nProgram Version", version)
+
+	fmt.Println("Var age type is", reflect.TypeOf(age))
 }
