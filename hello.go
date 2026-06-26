fc78733d3610b8851fdb33d9583f1324bf73a270
Artur <olivaovso@gmail.com>
2026-06-25 23:44:47 -0300

    http-verification

--- hello.go
+++ hello.go
@@ -17,10 +17,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
+
+	//RETURN TWO OR MORE VAR -> MAIN CODE
+	/*
+		name, age := returnNameAndAge()
+		fmt.Println(name, "is", age, "years old")
+	*/
 
 	sayGreeting()
 
@@ -29,19 +36,28 @@ func main() {
 	command := readVar()
 
 	switch command {
-	case 1:
-		fmt.Println("Monitoring")
-	case 2:
-		fmt.Println("System initialized and are working fine")
 	case 0:
 		fmt.Println("Exiting")
 		os.Exit(0)
+	case 1:
+		startMonitoringSystem()
+	case 2:
+		fmt.Println("System initialized and are working fine")
 	default:
 		fmt.Println("Esta opção não existe")
 		os.Exit(-1)
 	}
 
 }
+
+//RETURN TWO OR MORE VAR -> FUNC
+/*
+func returnNameAndAge() (string, int) {
+	name := "Artur"
+	age := 18
+	return name, age
+}
+*/
 
 func sayGreeting() {
 	name := "Artur"
@@ -63,4 +79,17 @@ func displayMenu() {
 	fmt.Println("1 - Monitoring System")
 	fmt.Println("2 - Logs")
 	fmt.Println("0 - Exit")
+}
+
+func startMonitoringSystem() {
+	fmt.Println("Monitoring")
+
+	site := "https://www.fiap.com.br/"
+	resp, _ := http.Get(site)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site is operating")
+	} else {
+		fmt.Println("Site is inoperating")
+	}
 }
